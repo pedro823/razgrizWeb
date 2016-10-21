0aeea4420c43fb175f16e5660e46f901a1647494
pedro823 <grey.pedrinho@gmail.com>
2016-10-21 20:10:01 -0200

    added one more post

--- js/controllers/PostsController.js
+++ js/controllers/PostsController.js
@@ -5,6 +5,11 @@ function($scope) {
             title: "This part now features angularJS!",
             text: "Now the post list is dynamic, so that it'll auto-grow as I make more posts.",
             date: new Date('2016', '9', '21')
+        },
+        {
+            title: "First Post",
+            text: "First post of latestProj",
+            date: new Date('2016', '9', '1')
         }
     ];
 }]);
