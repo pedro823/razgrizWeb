527609444567e1b808d778394551350d63602bf9
pedro823 <grey.pedrinho@gmail.com>
2016-10-23 11:42:56 -0200

    Makes angular work dinamically on index.html

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -4,3 +4,29 @@ function($scope) {
         lastChange: new Date('2016', '9', '21')
     };
 }]);
+app.controller('PostList', ['$scope',
+function($scope) {
+    $scope.postTexts = [
+        {
+            title: "This part now features angularJS!",
+            text: "Now the post list is dynamic, so that it'll auto-grow as I make more posts.",
+            date: new Date('2016', '9', '21'),
+        },
+        {
+            title: "First Post",
+            text: "First post of latestProj",
+            date: new Date('2016', '9', '1'),
+        }
+    ];
+    $scope.openSec = function(event) {
+        var obj = event.target.nextElementSibling;
+        if(obj.className.indexOf("w3-show") == -1) {
+            obj.className += " w3-show";
+            obj.previousElementSibling.className = obj.previousElementSibling.className.replace(" w3-black", " w3-red");
+        }
+        else {
+            obj.className = obj.className.replace(" w3-show", "");
+            obj.previousElementSibling.className = obj.previousElementSibling.className.replace(" w3-red", " w3-black");
+        }
+    };
+}]);
